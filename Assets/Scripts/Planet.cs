c0598908ead651cc69770544fd464748ddb08206
Suleman Aziz <sulemanaz548@gmail.com>
2025-12-25 14:16:07 -0500

    Implemented: - Terrain + Bathymetry Data Map. - Terrain mesh coloring.
    ToDo:
    - Identify plate boundary on mesh.
    - Integrate ML to move plates.

--- Assets/Scripts/Planet.cs
+++ Assets/Scripts/Planet.cs
@@ -1,14 +1,32 @@
-using System.Numerics;
 using UnityEngine;
+using System.IO;
 
 public class Planet : MonoBehaviour
 {
     [Range(2, 256)]
     public int resolution = 10;
 
+    [Range (1f,3f)]
+    public float WATERLEVEL = 1f;
+
+    [Range (2f, 5f)]
+    public float MountainLevel = 2.25f;
+
+    // Heightmap placed in Assets/Resources/TopoHeight.png (name without extension)
+    public Texture2D OceanheightMap;
+    public Texture2D heightMap;
+
+    [Range(0f, 1f)]
+    public float Oceanelevation = 0.15f;
+    [Range(0f, 1f)]
+    public float Topographyelevation = 0.15f;
+
+
     [SerializeField, HideInInspector]
     MeshFilter[] meshFilters;
     TerrainFaces[] terrainFaces;
+    // Material that supports vertex colors (reuse for all faces)
+    static Material vertexColorMaterial;
     void OnValidate()
     {
         Init();
@@ -16,6 +34,17 @@ public class Planet : MonoBehaviour
     }
     void Init()
     {
+        // Load Resources heightmap if not already set in inspector
+        if (OceanheightMap == null)
+        {
+            OceanheightMap = Resources.Load<Texture2D>("BathyProcessed");
+        }
+
+        if(heightMap == null)
+        {
+            heightMap = Resources.Load<Texture2D>("TopoHeight");
+        }
+
         if (meshFilters == null || meshFilters.Length == 0)
         {
             meshFilters = new MeshFilter[6];
@@ -30,15 +59,13 @@ public class Planet : MonoBehaviour
             {
                 GameObject meshObj = new GameObject("mesh");
                 meshObj.transform.parent = transform;
-
-                meshObj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Standard"));
-                
+ 
                 meshFilters[i] = meshObj.AddComponent<MeshFilter>();
 
                 meshFilters[i].sharedMesh = new Mesh();
             }
 
-            terrainFaces[i] = new TerrainFaces(meshFilters[i].sharedMesh, resolution, directions[i]);
+            terrainFaces[i] = new TerrainFaces(meshFilters[i].sharedMesh, resolution, directions[i], OceanheightMap, heightMap, Oceanelevation, Topographyelevation, WATERLEVEL, MountainLevel);
         }
     }
 
--- Assets/Scripts/TerrainFaces.cs
+++ Assets/Scripts/TerrainFaces.cs
@@ -1,5 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-
 public class TerrainFaces
 {
     Mesh mesh;
@@ -7,8 +9,30 @@ public class TerrainFaces
     Vector3 localUp;
     Vector3 axisA;
     Vector3 axisB;
+    Texture2D OceanheightMap;
+    Texture2D heightMap;
 
-    public TerrainFaces(Mesh m, int res, Vector3 up)
+    float OceanheightMultiplier;
+    float heightMultiplier;
+
+    float WATERLEVEL;
+    float MountainLevel;
+
+
+    public static Vector3 PointOnUnitCubeToPointOnUnitSphere(Vector3 p)
+    {
+        float x2 = p.x * p.x;
+        float y2 = p.y * p.y;
+        float z2 = p.z * p.z;
+
+        float x = (float)(p.x * Math.Sqrt(1 - (y2+z2)/2 + (y2*z2)/3 ) ) ;
+        float y = (float)(p.y * Math.Sqrt(1 - (z2+x2)/2 + (z2*x2)/3 ) ) ;
+        float z = (float)(p.z * Math.Sqrt(1 - (x2+y2)/2 + (x2*y2)/3 ) ) ;
+
+        return new Vector3(x,y,z);
+    }
+    
+    public TerrainFaces(Mesh m, int res, Vector3 up, Texture2D Oceanheightmap, Texture2D heightMap = null, float OceanheightMultiplier = 0f, float heightMultiplier = 0f, float WATERLEVEL = 1f, float MountainLevel = 2.25f)
     {
         mesh = m;
         resolution = res;
@@ -16,24 +40,72 @@ public class TerrainFaces
 
         axisA = new Vector3(localUp.y, localUp.z, localUp.x);
         axisB = Vector3.Cross(localUp, axisA);
+
+        this.OceanheightMap = Oceanheightmap;
+        this.heightMap = heightMap;
+
+        this.OceanheightMultiplier = OceanheightMultiplier;
+        this.heightMultiplier = heightMultiplier;
+        this.WATERLEVEL = WATERLEVEL;
+        this.MountainLevel = MountainLevel;
     }
+
 
     public void ConstructMesh()
     {
         Vector3[] vertices = new Vector3[resolution * resolution];
+        Color[] colors = new Color[resolution * resolution];
         int[] triangles = new int[(resolution - 1) * (resolution - 1) * 6];
         int triIndex = 0;
-
+ 
         for (int y = 0; y < resolution; y++)
         {
             for (int x = 0; x < resolution; x++)
             {
                 int i = x + y * resolution;
-                Vector2 percent = new Vector2(x, y) / (resolution - 1);
-                Vector3 pointOnUnitCube = localUp + (percent.x - 0.5f) * 2 * axisA + (percent.y - 0.5f) * 2 * axisB;
-                Vector3 pointOnUnitSphere = pointOnUnitCube.normalized;
-                vertices[i] = pointOnUnitSphere;
+                UnityEngine.Vector2 percent = new UnityEngine.Vector2(x, y) / (resolution - 1);
+                UnityEngine.Vector3 pointOnUnitCube = localUp + (percent.x - 0.5f) * 2 * axisA + (percent.y - 0.5f) * 2 * axisB;
 
+                UnityEngine.Vector3 pointOnUnitSphere = PointOnUnitCubeToPointOnUnitSphere(pointOnUnitCube);
+ 
+                // Sample Bathymetry heightmap and Topography heightmap then displace radially
+                
+                if (OceanheightMap != null || heightMap != null)
+                {
+                    var coord = GeoMaths.PointToCoordinate(pointOnUnitSphere);
+
+                    float u = (coord.longitude / (Mathf.PI * 2f)) + 0.5f;
+                    float v = (coord.latitude  / Mathf.PI) + 0.5f;
+
+                    float sampleO = OceanheightMap.GetPixelBilinear(u, v).r; // assume grayscale
+                    float radiusO = 1f + sampleO * OceanheightMultiplier;
+
+                    float sample = heightMap.GetPixelBilinear(u,v).r;
+                    float radius = 1f + sample * heightMultiplier;
+
+                    vertices[i] = pointOnUnitSphere * (radiusO + radius);
+
+                    
+                    if(radiusO + radius > WATERLEVEL)
+                    {
+                        if (radiusO + radius > MountainLevel)
+                        {
+                            colors[i] = Color.whiteSmoke;
+                        }
+                        else
+                        colors[i] = Color.lightGreen;
+                    }
+                    else
+                    {
+                        colors[i] = Color.softBlue;
+                    }
+                }
+                else
+                {
+                    vertices[i] = pointOnUnitSphere;
+                    colors[i] = Color.softBlue;
+                }
+ 
                 if (x != resolution - 1 && y != resolution - 1)
                 {
                     //Clockwise triangle coordinates on vertices
@@ -51,8 +123,11 @@ public class TerrainFaces
         }
 
         mesh.Clear();
-        mesh.vertices = vertices; //Need to map Lat/Long to each vertex point on Sphere
+
+        mesh.vertices = vertices;
         mesh.triangles = triangles;
+        mesh.colors = colors;
+
         mesh.RecalculateNormals();
     }
 }
