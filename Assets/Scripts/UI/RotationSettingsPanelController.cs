614fd06798f61430fb628e1db4ce49b8d340819c
Suleman Aziz <sulemanaz548@gmail.com>
2026-06-08 18:45:35 -0400

    Rotation Menu Script Rewritten. Working on UI

--- Assets/Scripts/UI/RotationSettingsPanelController.cs
+++ Assets/Scripts/UI/RotationSettingsPanelController.cs
@@ -5,13 +5,23 @@ using TMPro;
 public class RotationController: MonoBehaviour
 {
     public GlobeRotation Rotator;
+    public Planet planet;
     public TextMeshProUGUI RotationSpeedValueLabel;
-    public UnityEngine.UI.Slider RotationValueSlider;
-    private int rotationvalue;
+    public TextMeshProUGUI AxialTiltValueLabel;
+    
+    public UnityEngine.UI.Slider RotationSpeedSlider;
+    public UnityEngine.UI.Slider AxialTitlValueSlider;
+
+    private int Default_rotationspeed;
+    private float Default_tiltvalue;
+    private Vector3 Default_GlobePosition;
 
     void Start()
     {
         Hide();
+        Default_rotationspeed = Rotator.Speed;
+        Default_tiltvalue = Rotator.tiltAngle;
+        Default_GlobePosition = planet.transform.position;
     }
     public void Show()
     {
@@ -23,8 +33,24 @@ public class RotationController: MonoBehaviour
         gameObject.SetActive(false);
     }
 
-    public void ChangeRotationSpeed(int val)
+    public void ChangeRotationSpeed()
     {
-        
+        Rotator.Speed = (int) RotationSpeedSlider.value;
+        RotationSpeedValueLabel.text = RotationSpeedSlider.value.ToString();
     }
+
+    public void ChangeAxialTilt()
+    {
+        Rotator.tiltAngle = AxialTitlValueSlider.value;
+        AxialTiltValueLabel.text = AxialTitlValueSlider.value.ToString();
+    }
+
+    public void ResetSettings()
+    {
+        RotationSpeedSlider.value = Default_rotationspeed;
+        AxialTitlValueSlider.value = Default_tiltvalue;
+        planet.transform.position = Default_GlobePosition;
+        Hide();
+    }
+   
 }
